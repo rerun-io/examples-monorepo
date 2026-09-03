1a5c6e598197bce076717fbd26e6f07121c4e195
pablo vela <pablovela5620@gmail.com>
2026-09-03 02:03:34 -0500

    monoprior: correct the X-Lens upstream URL in the vendored docstring

--- packages/monoprior/monopriors/third_party/xlens/__init__.py
+++ packages/monoprior/monopriors/third_party/xlens/__init__.py
@@ -1,6 +1,6 @@
 """Vendored X-Lens inference subset.
 
-Upstream: https://github.com/henry123-boy/X-Lens
+Upstream: https://github.com/zhouhengamerica/XLens
 Revision: ``e6bdf2f66b26a9e4ef1663feaaf7e4618e1e8f7d``
 Code license: Apache-2.0; see ``LICENSE``.
 Released weights: Hugging Face ``henryzhou998/X-Lens`` at revision
